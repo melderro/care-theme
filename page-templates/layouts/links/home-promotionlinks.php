8ed0adc34b11a7ca754fe627148ec0585909038e
melderro <melanie@designfwd.com>
2018-12-10 01:37:13 -0500

    home promotional links setup and style

--- page-templates/layouts/links/home-promotionlinks.php
+++ page-templates/layouts/links/home-promotionlinks.php
@@ -1,28 +1,28 @@
 <?php // Home Promotional Links ?>
 <div class="o-homePromotionalLinks">
-  <div class="o-homePromotionalLinks__Block homePromotionalData">
-    <div class="o-homePromotionalLinks__text">  
+  <div class="m-homePromotionalLinks__Block homePromotionalData">
+    <div class="m-homePromotionalLinks__text">  
       72,000 are estimated to have died from a drug overdose in 2017.
     </div>
-    <div class="o-homePromotionalLinks__source">  
+    <div class="m-homePromotionalLinks__source">  
       Source Citation
     </div>
   </div>
-  <div class="o-homePromotionalLinks__Block homePromotionalCaseStudy">
-    <img class="o-homePromotionalLinks__image" src="<?php echo get_acf_image( get_field('intro_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
-    <h3 class="o-homePromotionalLinks__imagetitle">Amy's Story</h3>  
-    <div class="o-homePromotionalLinks__imagetext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tincidunt velit, at fermentum diam. </div>  
+  <div class="m-homePromotionalLinks__Block homePromotionalCaseStudy">
+    <img class="m-homePromotionalLinks__image" src="<?php echo get_acf_image( get_field('intro_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
+    <h3 class="m-homePromotionalLinks__imagetitle">Amy's Story</h3>  
+    <div class="m-homePromotionalLinks__imagetext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tincidunt velit, at fermentum diam. </div>  
     <a class="a-roundLink o-CaseStudyLinks">Learn More</a>
 </div>
   
-  <div class="o-homePromotionalLinks__Block homePromotionalImage">
-    <img class="o-homePromotionalLinks__image" src="<?php echo get_acf_image( get_field('intro_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
+  <div class="m-homePromotionalLinks__Block homePromotionalImage">
+    <img class="m-homePromotionalLinks__image" src="<?php echo get_acf_image( get_field('intro_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
   </div>
-  <div class="o-homePromotionalLinks__Block homePromotionalData">
-    <h2 class="o-homePromotionalLinks__title">  
+  <div class="m-homePromotionalLinks__Block homePromotionalData">
+    <h2 class="m-homePromotionalLinks__title">  
       Featured Event
     </h2>
-    <div class="o-homePromotionalLinks__text">  
+    <div class="m-homePromotionalLinks__text">  
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tincidunt velit, at fermentum diam. 
     </div>
     <a class="a-roundLink o-PromotionalLinks">Learn More</a>
--- page-templates/layouts/media/topbanner.php
+++ page-templates/layouts/media/topbanner.php
@@ -1,3 +1,5 @@
 <?php // Top Banner ?>
 <div class="o-pageBanner">
+<img class="o-pageBanner__image" src="<?php echo get_acf_image( get_field('intro_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
+
 </div>
