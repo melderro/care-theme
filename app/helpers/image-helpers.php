c1fa7ec94fe94a94152ab4291f12a631faebf618
sajison <saji@designfwd.com>
2018-12-19 14:51:31 -0500

    update topbanner

--- app/helpers/image-helpers.php
+++ app/helpers/image-helpers.php
@@ -1,14 +1,4 @@
 <?php
-<<<<<<< HEAD
-    //return image
-    function get_acf_image ( $image_id, $size, $default) {
-        if(is_numeric($image_id)) {
-            $image = wp_get_attachment_image_src($image_id, $size);
-            return $image[0];
-        }else
-            return $default;
-    }
-=======
 /** return image */
 
 function get_acf_image($image_id, $size, $default = '')
@@ -31,4 +21,3 @@ function get_acf_image_srcset($image_id, $class='', $size = array('2600w', '1300
     }
 
 }
->>>>>>> js and responsive top banner image
