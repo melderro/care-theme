2abd52dfd252e7fd633d33c94f66808d687118dc
sajison <saji@designfwd.com>
2019-04-30 10:06:22 -0400

    newsletter updates

--- app/helpers/newsletter-form.php
+++ app/helpers/newsletter-form.php
@@ -12,39 +12,10 @@ function process_news_letter_form(){
 
   if(filter_var($email, FILTER_VALIDATE_EMAIL)) :
     if(is_email($email)) :
-      //create email messages
-//       echo ("$email safe to send");
-//       $admin_to = "saji@designfwd.com";
-//       $subject = "Thank you for subscribing to our newsletter";
-//       $admin_message = <<<ADMINEMAIL
-//       <html>
-//           <head>
-//               <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-//           </head>
-//           <body>
-//             $email has joined the mailing list.
-
-//           </body>
-//         </html>
-// ADMINEMAIL;
-
-//       $user_message = <<<USEREMAIL
-//       <html>
-//           <head>
-//               <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-//           </head>
-//           <body>
-//             Thank you for joining the mailing list.
-
-//           </body>
-//         </html>
-// USEREMAIL;
-
-      //add entry to gravity forms
+      //add entry to gravity forms, send out notifications and process confirmations
       $form_id = 1;
       $input_values['input_1'] = $email;
       $result = GFAPI::submit_form($form_id, $input_values);
-      var_dump ($result);
     endif;
   else :
     echo ("no good");
