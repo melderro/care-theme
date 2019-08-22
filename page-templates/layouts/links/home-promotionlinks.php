280e0e98036a91f42e017cc479dcabba060d193d
sajison <saji@designfwd.com>
2019-08-22 14:42:33 -0400

    homepage testimonial edits

--- page-templates/layouts/links/home-promotionlinks.php
+++ page-templates/layouts/links/home-promotionlinks.php
@@ -1,34 +1,53 @@
-<?php // Home Promotional Links ?>
+<?php // Home Promotional Links
+  $home_case_study_testimonial = get_field('home_case_study_testimonial');
+  $featured_image_id = get_post_thumbnail_id($home_case_study_testimonial->ID);
+
+  $event = get_field('home_event_link');
+  $event_featured_image_id = get_post_thumbnail_id($event->ID);
+?>
 <div class="o-homePromotionalLinks">
   
   <div class="m-homePromotionalLinks__Block homePromotionalData homePromotionalCaseStudy">
     <div class="m-homePromotionalLinks__text">  
-      <?php the_field('home_promotion_links_text'); ?>
+      <?php the_field('data_statistic_content', 'option'); ?>
     </div>
     <div class="m-homePromotionalLinks__source">  
-      <?php the_field('home_promotion_links_source'); ?>
+      <?php the_field('data_statistic_content_source', 'option'); ?>
     </div>
   </div>
-  <a class="m-homePromotionalLinks__Block homePromotionalImage homePromotionalCaseStudy" href="<?php the_field('home_case_study_link'); ?>">
-      <img class="m-homePromotionalLinks__image" src="<?php echo get_acf_image( get_field('home_case_study_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
+  <a class="m-homePromotionalLinks__Block homePromotionalImage homePromotionalCaseStudy" href="<?php echo get_the_permalink($home_case_study_testimonial->ID); ?>">
+      
+  <img class="m-homePromotionalLinks__image lazyload lazyload--blurUp"
+    alt="<?php echo get_post_meta($featured_image_id, '_wp_attachment_image_alt', true); ?>"
+    data-sizes="auto"
+    src="<?php echo get_the_post_thumbnail_url( $home_case_study_testimonial->ID,  $size = 'preload' ); ?>"
+    data-srcset="<?php echo wp_get_attachment_image_srcset( $featured_image_id) ?>"
+  />
+
+
     <div class="m-homePromotionalLinks__ContentContainer">
-      <h3 class="m-homePromotionalLinks__imagetitle"><?php the_field('home_case_study_title'); ?></h3>  
-      <div class="m-homePromotionalLinks__imagetext"><?php the_field('home_case_study_text'); ?></div>  
-      <span class="a-roundLink o-homePromotionalCaseStudyLink"> <?php the_field('home_case_study_link_title'); ?></span>
+      <h3 class="m-homePromotionalLinks__imagetitle"><?php echo $home_case_study_testimonial->post_title; ?></h3>  
+      <div class="m-homePromotionalLinks__imagetext"><?php echo $home_case_study_testimonial->post_excerpt; ?></div>  
+      <span class="a-roundLink o-homePromotionalCaseStudyLink"> Read More </span>
     </div>
   </a>
   
   <div class="m-homePromotionalLinks__Block homePromotionalImage homePromotionalEvent">
-    <img class="m-homePromotionalLinks__image" src="<?php echo get_acf_image( get_field('home_event_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/>
+  <img class="m-homePromotionalLinks__image lazyload lazyload--blurUp"
+    alt="<?php echo get_post_meta($event_featured_image_id, '_wp_attachment_image_alt', true); ?>"
+    data-sizes="auto"
+    src="<?php echo get_the_post_thumbnail_url( $event->ID,  $size = 'preload' ); ?>"
+    data-srcset="<?php echo wp_get_attachment_image_srcset( $event_featured_image_id) ?>"
+  />  
   </div>
   
   <a class="m-homePromotionalLinks__Block homePromotionalData homePromotionalEvent">
     <h2 class="m-homePromotionalLinks__title">  
-      <?php the_field('home_event_title'); ?>
+      <?php echo $event->post_title ?>
     </h2>
     <div class="m-homePromotionalLinks__text">  
-      <?php the_field('home_event_text'); ?>
+      <?php echo $event->post_excerpt; ?>
     </div>
-    <span class="a-roundLink o-homePromotionalEventLink">Learn More</span>
+    <a href="<?php echo get_the_permalink($event->ID); ?>"><span class="a-roundLink o-homePromotionalEventLink">Learn More</span></a>
   </a>
 </div>
