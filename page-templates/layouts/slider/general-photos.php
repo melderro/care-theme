bafd68b45781a19e17baab4d6fce5e2a466b9ee4
sajison <saji@designfwd.com>
2019-09-03 22:55:07 -0400

    finalize blog landing page html

--- page-templates/layouts/slider/general-photos.php
+++ page-templates/layouts/slider/general-photos.php
@@ -1,16 +1,20 @@
-<?php // Genearl Body paragraph content ?>
+<?php // Genearl Body paragraph content 
+  if(have_rows('general_photo_slider')):
+?>
 <div class="o-generalbodyPhotos">
-  <ul class="m-generalbodyPhotos">
+  <ul class="m-generalbodyPhotos" class="preload">
     <?php
-      if(have_rows('general_photo_slider')):
-        while(have_rows('general_photo_slider')):the_row();
+      while(have_rows('general_photo_slider')):the_row();
     ?>
     <li>
       <img class="m-generalbodyPhotos__image" src="<?php echo get_acf_image( get_sub_field('general_photo_slider_image'), '540w', 'http://via.placeholder.com/588x216?text=logo'); ?> "/> 
     </li>
     <?php
-        endwhile;
-      endif;
+      endwhile;
     ?>
   </ul>
 </div>
+</li>
+<?php
+  endif;
+?>
