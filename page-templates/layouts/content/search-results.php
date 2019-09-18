a24f7af5cc2d7834c87edfca76a83c97d948722e
sajison <saji@designfwd.com>
2019-09-18 15:27:39 -0400

    update search code

--- page-templates/layouts/content/search-results.php
+++ page-templates/layouts/content/search-results.php
@@ -1,24 +1,11 @@
 <?php // News Categories Filter ?>
 <div class="o-articleGrid">
-<?php
-  $s = get_search_query();
-  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-
-  $args = array( 
-    'post_type' => 'any',
-    'posts_per_page' => 5, 
-    'paged' => $paged,
-    'post_status' => 'publish',
-    's' => $s,
-    );
-  $posts = new WP_Query($args); 
-?>
   <ul class="m-articleGridBlock">
     <?php 
-  echo "Search found ".$wp_query->found_posts."results";
+  echo "Search found ".$wp_query->found_posts." results";
 
-      while($posts->have_posts()) :
-        $posts->the_post();
+      while(have_posts()) : the_post();
+        //$posts->the_post();
         $categories = get_the_category();    
         $meta_date = get_the_date('M d, Y');
     ?>
@@ -60,11 +47,8 @@
   <?php 
     remove_filter( 'the_category', 'no_links' );
     // Bottom pagination (pagination arguments)
-    echo "<div class='page-nav-container'>" . paginate_links(array(
-      'total' => $posts->max_num_pages,
-      'prev_text' => __('<'),
-      'next_text' => __('>')
-    )) . "</div>";
-
-  
-  ?>
+    the_posts_pagination( array(
+      'mid_size' => 2,
+      'prev_text' => __( 'Previous', 'textdomain' ),
+      'next_text' => __( 'Next', 'textdomain' ),
+  ) ); ?>
