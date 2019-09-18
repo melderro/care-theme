49abe6a57e1ae33be641c9704429f0ecb6408154
sajison <saji@designfwd.com>
2019-09-18 13:51:49 -0400

    in progress: search pagination

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ elseif( is_singular(array ('post', 'testimonial'))) :
   get_template_part( 'page-templates/template-newsLanding' );
 elseif(is_404()) :
   get_template_part( 'page-templates/template-404' );
+elseif(is_search()) :
+  get_template_part( 'page-templates/template-search' );  
 else:
   get_template_part( 'page-templates/template-generalLanding' );
 endif;
---
+++ page-templates/layouts/content/search-results.php
@@ -0,0 +1,68 @@
+<?php // News Categories Filter ?>
+<div class="o-articleGrid">
+<?php
+  $s = get_search_query();
+  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+  $args = array( 
+    'post_type' => 'any',
+    'posts_per_page' => 5, 
+    'paged' => $paged,
+    's' => $s,
+    );
+  $posts = new WP_Query($args); 
+?>
+  <ul class="m-articleGridBlock">
+    <?php 
+  echo "Search found ".$wp_query->found_posts."results";
+
+      while($posts->have_posts()) :
+        $posts->the_post();
+        $categories = get_the_category();    
+        $meta_date = get_the_date('M d, Y');
+    ?>
+    <li>
+      <div class="m-articleGridBlock__imageContainer">
+      <?php 
+        if(get_field('intro_image')): 
+          echo get_acf_image_srcset( get_field('intro_image'), 'lazyload m-articleGridBlock__imageContainer'); 
+        else :
+          echo get_acf_image_srcset( get_field('news_intro_image', 'option'), 'lazyload m-articleGridBlock__imageContainer'); 
+        endif;
+      ?>
+      </div>
+      <div class="m-articleGridBlock__titleContainer">
+        <h3 class="o-introContent__subtitle">
+          <?php the_category(', '); ?>
+        </h3>
+        <a href="<?php the_permalink();?>">
+        <h1 class="o-introContent__title">
+          <?php the_title(); ?>
+        </h1>
+        </a>
+        <div class="m-articleGridBlock__metadata">
+          <?php get_svg('clock-light');?>
+          <span><?php echo $meta_date; ?></span>
+        </div>
+      </div>
+      <div class="m-articleGridBlock__contentContainer">
+        <div class="m-articleGridBlock__content">
+         <?php the_excerpt(); ?>
+        </div>
+      </div>
+    </li>
+    <?php 
+      endwhile;
+    ?>
+  </ul>
+</div>
+  <?php 
+    remove_filter( 'the_category', 'no_links' );
+    // Bottom pagination (pagination arguments)
+    echo "<div class='page-nav-container'>" . paginate_links(array(
+      'total' => $posts->max_num_pages,
+      'prev_text' => __('<'),
+      'next_text' => __('>')
+    )) . "</div>";
+
+  
+  ?>
