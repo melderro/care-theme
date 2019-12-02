2a4fc370bbcce4ca2612a262f3f942b1ca410779
sajison <saji@designfwd.com>
2019-12-02 15:31:40 -0500

    gtm & footer blog source

--- page-templates/layouts/meta/head.php
+++ page-templates/layouts/meta/head.php
@@ -28,8 +28,15 @@
   <?php
     endif;
   ?>
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MB58JC8');</script>
+<!-- End Google Tag Manager -->
   <?php // WordPress head() function
+  
     wp_head();
   ?>
 </head>
