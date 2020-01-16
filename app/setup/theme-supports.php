ae0f37defb17310310b8a0e52a92bcd66fce6520
sajison <saji@designfwd.com>
2020-01-16 12:28:06 -0500

    add acf previews

--- app/setup/theme-supports.php
+++ app/setup/theme-supports.php
@@ -12,3 +12,18 @@ function fwd_supports() {
   add_theme_support('align-wide');
 }
 add_action( 'after_setup_theme', 'fwd_supports' );
+
+/* 
+   Debug preview with custom fields 
+*/ 
+
+add_filter('_wp_post_revision_fields', 'add_field_debug_preview');
+function add_field_debug_preview($fields){
+   $fields["debug_preview"] = "debug_preview";
+   return $fields;
+}
+
+add_action( 'edit_form_after_title', 'add_input_debug_preview' );
+function add_input_debug_preview() {
+   echo '<input type="hidden" name="debug_preview" value="debug_preview">';
+}
